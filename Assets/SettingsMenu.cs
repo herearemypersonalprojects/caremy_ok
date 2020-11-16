5f9fad890f1c7fc712091fae25cad4307606c4c6
quoc-anh.le <ing.dev.java@gmail.com>
2020-11-16 20:28:57 +0100

    Main menu 1

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -5,8 +5,20 @@ public class SettingsMenu : MonoBehaviour
 {
 	public AudioMixer audioMixer;
 
+	Resolution[] resolutions;
+
+	public void Start()
+	{
+		resolutions = Screen.resolutions;
+	}
+
     public void SetVolume(float volume)
     {
     	audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+    	Screen.fullScreen = isFullScreen;
     }
 }
