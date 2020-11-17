30bc7a7f94afa8dd6ca2d75eefe71feaf6e5f9f2
quoc-anh.le <ing.dev.java@gmail.com>
2020-11-17 16:30:31 +0100

    caca

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,15 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
 	public AudioMixer audioMixer;
+
+	public Dropdown resolutionDropdown;
 
 	Resolution[] resolutions;
 
 	public void Start()
 	{
 		resolutions = Screen.resolutions;
+		resolutionDropdown.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		for (int i = 0; i < resolutions.Length; i++)
+		{
+			string option = resolutions[i].width + "x" + resolutions[i].height;
+			options.Add(option);
+		}
+
+		resolutionDropdown.AddOptions(options);
 	}
 
     public void SetVolume(float volume)
